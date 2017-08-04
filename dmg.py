a3bcafcaee31d97d12bc064dc2cddfcac6d6c674
mysticfyst <mysticfyst@gmail.com>
2017-08-04 11:20:09 +0530

    Changelog: Puts the downloaded files in an organized way in /home/<USERNAME>/manga/<MANGA_NAME>/<CHAPTER>/ but only works on linux machines

--- dmg.py
+++ dmg.py
@@ -1,13 +1,15 @@
 #! /usr/bin/python
 import urllib2
-import urllib
+import os
+import errno
 import json
 import requests
 from bs4 import BeautifulSoup
+
 INFO_FILE='manga_info.json'
-#manga='one-piece'
-#chapter =871
-#page=18
+DEFAULT_LINUX_PATH='/home/'+os.getlogin()
+#DEFAULT_WINDOWS_PATH='C:\\Manga\\'
+
 with open(INFO_FILE, 'r') as f:
 	pyDict=json.load(f)
 noOfMangas= len(pyDict)
@@ -16,27 +18,45 @@ for i in range(0,noOfMangas,1):
 	chapter=pyDict[i]['chapter']
 	page =1
 	checker=0
+	#try creating a directory for the manga if it doesn't exist
+	mangaDirectory=DEFAULT_LINUX_PATH+'/manga/'+manga
+	try:
+		os.makedirs(mangaDirectory)
+	except OSError as e:
+		if e.errno != errno.EEXIST:
+			raise
+	chapterDirectory='null'
 	while checker != 1:
 		pageDirectory=manga+'/'+str(chapter)+'/'+str(page)
 		link='http://www.mangapanda.com/'+pageDirectory
-		print link
 		try:
 			pageLink=urllib2.urlopen(link)
+			#print link
+			#try creating a directory for the current chapter in the manga if it doesn't exist
+			if page == 1:
+				try:
+					chapterDirectory=mangaDirectory+'/'+str(chapter)
+					os.makedirs(chapterDirectory)
+					print "Downloading chapter "+str(chapter)+" of "+manga+"."
+				except OSError as e:
+					if e.errno != errno.EEXIST:
+						raise
+			#the following will run on reaching the end of the chapter
 		except urllib2.HTTPError, e:
-			print(e.code)
+			#print(e.code)
 			chapter = chapter + 1;
 			pyDict[i]['chapter'] = chapter
 			page = 1
 			continue
 		soup = BeautifulSoup(pageLink, "lxml")
 		imageLinkString=soup.find('img', id='img')
-		#print imageLinkString
+		#if imageLinkString is not None then it contains an image which will be downloaded
 		if str(imageLinkString) != 'None':
 			checker = 0;
 			imageLink=imageLinkString.get("src")
 			#somehow the following line doesn't work
 			#urllib.urlretrieve(imageLink, "image.jpg")
-			with open(manga+'-'+str(chapter)+'-'+str(page)+'.jpg', 'wb') as handle:
+			with open(chapterDirectory+'/'+str(page)+'.jpg', 'wb') as handle:
 				response = requests.get(imageLink, stream=True)
 
 				if not response.ok:
@@ -50,7 +70,8 @@ for i in range(0,noOfMangas,1):
 			page = page+1;
 			print imageLink
 		else :
-			#the manga has not been released yet or has been concluded
+			#imageLinkString prints None if trying to access a n+1th chapter of manga with n chapters
+			print "Chapter "+str(chapter+1)+" of "+manga+" has been concluded or not yet released."
 			with open(INFO_FILE, 'w') as out:
     				json.dump(pyDict, out)
 			checker =1
