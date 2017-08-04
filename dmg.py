b4ae31ac734cf377123cfef242993b74a55141e6
mysticfyst <mysticfyst@gmail.com>
2017-08-04 08:51:53 +0530

    Changelog: 1. Added support for multiple mangas 2. Information about the mangas is saved in the JSON file along with the current chapter. 3. A new manga can be added by changing the contents of the file manga_info.json

--- dmg.py
+++ dmg.py
@@ -1,45 +1,56 @@
 #! /usr/bin/python
 import urllib2
 import urllib
+import json
 import requests
 from bs4 import BeautifulSoup
-manga='one-piece'
-chapter =871
-page=18
-checker =0
-while checker != 1:
-	pageDirectory=manga+'/'+str(chapter)+'/'+str(page)
-	link='http://www.mangapanda.com/'+pageDirectory
-	print link
-	try:
-		pageLink=urllib2.urlopen(link)
-	except urllib2.HTTPError, e:
-		print(e.code)
-		chapter = chapter + 1;
-		page = 1
-		continue
-	soup = BeautifulSoup(pageLink, "lxml")
-	imageLinkString=soup.find('img', id='img')
-	#print imageLinkString
-	if str(imageLinkString) != 'None':
-		checker = 0;
-		imageLink=imageLinkString.get("src")
-		#somehow the following line doesn't work
-		#urllib.urlretrieve(imageLink, "image.jpg")
-		with open(str(chapter)+'-'+str(page)+'.jpg', 'wb') as handle:
-			response = requests.get(imageLink, stream=True)
+INFO_FILE='manga_info.json'
+#manga='one-piece'
+#chapter =871
+#page=18
+with open(INFO_FILE, 'r') as f:
+	pyDict=json.load(f)
+noOfMangas= len(pyDict)
+for i in range(0,noOfMangas,1):
+	manga=pyDict[i]['manga']
+	chapter=pyDict[i]['chapter']
+	page =1
+	checker=0
+	while checker != 1:
+		pageDirectory=manga+'/'+str(chapter)+'/'+str(page)
+		link='http://www.mangapanda.com/'+pageDirectory
+		print link
+		try:
+			pageLink=urllib2.urlopen(link)
+		except urllib2.HTTPError, e:
+			print(e.code)
+			chapter = chapter + 1;
+			pyDict[i]['chapter'] = chapter
+			page = 1
+			continue
+		soup = BeautifulSoup(pageLink, "lxml")
+		imageLinkString=soup.find('img', id='img')
+		#print imageLinkString
+		if str(imageLinkString) != 'None':
+			checker = 0;
+			imageLink=imageLinkString.get("src")
+			#somehow the following line doesn't work
+			#urllib.urlretrieve(imageLink, "image.jpg")
+			with open(manga+'-'+str(chapter)+'-'+str(page)+'.jpg', 'wb') as handle:
+				response = requests.get(imageLink, stream=True)
 
-			if not response.ok:
-			    print response
+				if not response.ok:
+				    print response
 
-			for block in response.iter_content(1024):
-			    if not block:
-			        break
+				for block in response.iter_content(1024):
+				    if not block:
+					break
 
-			    handle.write(block)
-		page = page+1;
-		print imageLink
-	else :
-		#the manga has not been released yet or has been concluded
-		#TODO: save the chapter number to resume downloading when the chapter is released
-		checker =1
+				    handle.write(block)
+			page = page+1;
+			print imageLink
+		else :
+			#the manga has not been released yet or has been concluded
+			with open(INFO_FILE, 'w') as out:
+    				json.dump(pyDict, out)
+			checker =1
